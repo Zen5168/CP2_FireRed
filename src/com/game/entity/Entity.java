9e9fdbfb84c6d014eac3244025d16083c1b140e0
Zen5168 <jexkeanalfonso@gmail.com>
2026-05-14 14:41:19 +0800

    Created a new package named entity and new Classes named Entity & Player and also changed the tile size from 16x16 to 19x19

---
+++ src/com/game/entity/Entity.java
@@ -0,0 +1,7 @@
+package com.game.entity;
+
+public class Entity {
+    
+    public int x, y;
+    public int speed;
+}
--- src/com/game/gui/GamePanel.java
+++ src/com/game/gui/GamePanel.java
@@ -3,16 +3,17 @@ package com.game.gui;
 import javax.swing.*;
 import java.awt.*;
 import com.game.logic.*;
+import com.game.entity.*;
 
 public class GamePanel extends JPanel implements Runnable {
 
     //========================================
     // SCREEN SETTINGS
     //========================================
-    final int originalTileSize = 16; // 16x16 Tile
+    final int originalTileSize = 19; // 19x19 Tile
     final int scale = 3; // TILE SCALING 
 
-    final int tileSize = originalTileSize * scale; // MAKES THE TILE 48x48
+    public final int tileSize = originalTileSize * scale; // MAKES THE TILE 48x48
     final int maxScreenCol = 16; // 16 TILES HORIZONTALLY
     final int maxScreenRow = 12; // 16 TILES VERTICALLY
     final int screenWidth = tileSize * maxScreenCol; // 768 PIXELS
@@ -25,6 +26,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     //========================================
     // SET PLAYER'S DEFAULT LOCATION
@@ -87,15 +89,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void update() {
 
-        if (keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        } else if (keyH.downPressed == true) {
-            playerY += playerSpeed;
-        } else if (keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        } else if (keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     @Override
@@ -104,10 +98,8 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        
+        player.draw(g2);
 
         g2.dispose(); // DISPOSES GRAPHICS CONTEXT
     }
