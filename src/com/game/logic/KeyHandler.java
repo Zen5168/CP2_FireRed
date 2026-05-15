e6896512a2213e33915ff7e5290f8826c513ea07
Zen5168 <jexkeanalfonso@gmail.com>
2026-05-15 21:21:34 +0800

    Reduced player speed from 4 to 2 but added a new feature of running or sprinting by pressing the control button

--- src/com/game/logic/KeyHandler.java
+++ src/com/game/logic/KeyHandler.java
@@ -3,49 +3,55 @@ package com.game.logic;
 import java.awt.event.*;
 
 public class KeyHandler implements KeyListener {
-    
-    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed, ctrlPressed;
 
     @Override
     public void keyTyped(KeyEvent e) { // UNUSED BUT NEC
     }
-    
+
     @Override
     public void keyPressed(KeyEvent e) { // RUNS WHEN A KEY IS PRESSED
-        
+
         int code = e.getKeyCode();
-        
-        if(code == KeyEvent.VK_W) {
+
+        if (code == KeyEvent.VK_W) {
             upPressed = true;
         }
-        if(code == KeyEvent.VK_A) {
+        if (code == KeyEvent.VK_A) {
             leftPressed = true;
         }
-        if(code == KeyEvent.VK_S) {
+        if (code == KeyEvent.VK_S) {
             downPressed = true;
         }
-        if(code == KeyEvent.VK_D) {
+        if (code == KeyEvent.VK_D) {
             rightPressed = true;
         }
+        if (code == KeyEvent.VK_CONTROL) {
+            ctrlPressed = true;
+        }
     }
-    
+
     @Override
     public void keyReleased(KeyEvent e) { // RUNS WHEN A KEY IS RELEASED
-        
-                int code = e.getKeyCode();
-        
-        if(code == KeyEvent.VK_W) {
+
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
             upPressed = false;
         }
-        if(code == KeyEvent.VK_A) {
+        if (code == KeyEvent.VK_A) {
             leftPressed = false;
         }
-        if(code == KeyEvent.VK_S) {
+        if (code == KeyEvent.VK_S) {
             downPressed = false;
         }
-        if(code == KeyEvent.VK_D) {
+        if (code == KeyEvent.VK_D) {
             rightPressed = false;
-        } 
+        }
+        if (code == KeyEvent.VK_CONTROL) {
+            ctrlPressed = false;
+        }
     }
-    
+
 }
